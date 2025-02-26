70fe3d2cb93bef174f43b993419cf605da8a3d8b
Naammm <tagiangnamttg@gmail.com>
2025-02-27 01:57:23 +0700

    update

--- Practice_1/src/java/dao/MobileDAO.java
+++ Practice_1/src/java/dao/MobileDAO.java
@@ -5,10 +5,43 @@
  */
 package dao;
 
+import dto.MobileDTO;
+import java.util.List;
+
 /**
  *
  * @author Naammm
  */
-public class MobileDAO {
+public class MobileDAO implements IDAO<MobileDTO, String>{
+
+    @Override
+    public boolean create(MobileDTO entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<MobileDTO> readAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public MobileDTO readById(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(MobileDTO entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<MobileDTO> search(String searchTerm) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- Practice_1/src/java/dto/UserDTO.java
+++ Practice_1/src/java/dto/UserDTO.java
@@ -10,5 +10,57 @@ package dto;
  * @author Naammm
  */
 public class UserDTO {
+    private String userId;
+    private String password;
+    private String fullName;
+    private String role;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String userId, String password, String fullName, String role) {
+        this.userId = userId;
+        this.password = password;
+        this.fullName = fullName;
+        this.role = role;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" + "userId=" + userId + ", password=" + password + ", fullName=" + fullName + ", role=" + role + '}';
+    }
+    
     
 }
